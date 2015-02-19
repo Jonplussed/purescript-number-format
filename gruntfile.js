2fe121a01db6fc0fca43ee847a3bc5044946b83e
Jon Childress <jon@childr.es>
2015-02-19 11:42:27 -0600

    use grunt-shell to run purescript-test-unit specs

--- gruntfile.js
+++ gruntfile.js
@@ -3,27 +3,64 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+
+
+    // file locations
+
     srcFiles: [
       "src/**/*.purs",
       "bower_components/purescript-*/src/**/*.purs",
     ],
 
+    testFiles: [
+      "test/**/*.purs",
+      "<%=srcFiles%>"
+    ],
+
+    testOutput: "output/test.js",
+
+
+
+    // tasks
+
     clean:   ["output"],
-    pscMake: ["<%=srcFiles%>"],
     dotPsci: ["<%=srcFiles%>"],
+    pscMake: ["<%=srcFiles%>"],
+
+    psc: {
+      options: {
+        modules: 'Test.Main',
+        main: 'Test.Main'
+      },
+
+      test: {
+        src:  ["<%=testFiles%>"],
+        dest: "<%=testOutput%>"
+      }
+    },
 
     pscDocs: {
       readme: {
         src: "src/**/*.purs",
         dest: "README.md"
       }
+    },
+
+    shell: {
+      target: {
+        command: 'node "<%=testOutput%>"'
+      }
     }
 
   });
 
+
+
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-purescript");
+  grunt.loadNpmTasks("grunt-shell");
 
   grunt.registerTask("make", ["pscMake", "dotPsci", "pscDocs"]);
   grunt.registerTask("default", ["make"]);
+  grunt.registerTask("test", ["psc:test", "shell"]);
 };
