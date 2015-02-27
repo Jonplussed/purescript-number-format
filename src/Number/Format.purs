f39efc55c7bd1f411b97570440b3709620f9a6ed
Jon Childress <jon@childr.es>
2015-02-26 21:06:34 -0600

    extract as much commonality as won't break unification of Eff monads

--- src/Number/Format.purs
+++ src/Number/Format.purs
@@ -5,19 +5,24 @@ module Number.Format
 , toString
 ) where
 
-import Control.Monad.Eff (runPure)
-import Control.Monad.Eff.Exception (catchException)
+import Control.Monad.Eff (Eff (), runPure)
+import Control.Monad.Eff.Exception (Exception (), catchException)
 import Data.Maybe (Maybe (..))
 import Number.Format.Unsafe
 
 toExponential :: Number -> Number -> Maybe String
-toExponential scale num = runPure (catchException (return <<< const Nothing) (Just <$> unsafeToExponential scale num))
+toExponential scale num = runPure (errorAsNothing $ Just <$> unsafeToExponential scale num)
 
 toFixed :: Number -> Number -> Maybe String
-toFixed scale num = runPure (catchException (return <<< const Nothing) (Just <$> unsafeToFixed scale num))
+toFixed scale num = runPure (errorAsNothing $ Just <$> unsafeToFixed scale num)
 
 toPrecision :: Number -> Number -> Maybe String
-toPrecision prec num = runPure (catchException (return <<< const Nothing) (Just <$> unsafeToPrecision prec num))
+toPrecision prec num = runPure (errorAsNothing $ Just <$> unsafeToPrecision prec num)
 
 toString :: Number -> Number -> Maybe String
-toString radix num = runPure (catchException (return <<< const Nothing) (Just <$> unsafeToString radix num))
+toString radix num = runPure (errorAsNothing $ Just <$> unsafeToString radix num)
+
+-- private functions
+
+errorAsNothing :: forall eff a. Eff (err :: Exception | eff) (Maybe a) -> Eff eff (Maybe a)
+errorAsNothing = catchException (return <<< const Nothing)
